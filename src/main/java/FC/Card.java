1d5ab272b029e391238148e70af9fe144040c0d7
Ricki Peikes <rickipeikes@gmail.com>
2020-12-03 15:45:32 -0500

    added fields and basic methods to Card class

--- src/main/java/FC/Card.java
+++ src/main/java/FC/Card.java
@@ -1,2 +1,27 @@
-package FC;public class Card {
+package FC;
+
+public class Card {
+    private String term;
+    private String def;
+    private boolean mastered;
+
+    public String getDef() {
+        return def;
+    }
+
+    public void setDef(String def) {
+        this.def = def;
+    }
+
+    public String getTerm() {
+        return term;
+    }
+
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    public void toggleMastered() {
+        mastered = !mastered;
+    }
 }
