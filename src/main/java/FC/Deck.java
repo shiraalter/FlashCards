5f85451539516069c3438f0d4236c01a8d87c8c5
Ricki Peikes <rickipeikes@gmail.com>
2020-12-04 11:42:07 -0500

    started Deck class

--- src/main/java/FC/Deck.java
+++ src/main/java/FC/Deck.java
@@ -1,2 +1,21 @@
-package FC;public class Deck {
+package FC;
+
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck(){
+
+    }
+
+    public Deck(List<Card> cards){
+        this.cards = cards;
+    }
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+
 }
