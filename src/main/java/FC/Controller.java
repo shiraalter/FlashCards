392e65148980fbb024493222071ea520eded04ad
Ricki Peikes <rickipeikes@gmail.com>
2020-12-05 23:21:05 -0500

    beginning of db functionality

---
+++ src/main/java/FC/Controller.java
@@ -0,0 +1,41 @@
+package FC;
+
+import java.sql.*;
+
+public class Controller {
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+
+    /**
+     * select all rows in the warehouses table
+     */
+    public void selectAll(String table){
+        String sql = "SELECT * FROM " + table;
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" +
+                        rs.getString("name") + "\t" +
+                        rs.getDouble("capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
