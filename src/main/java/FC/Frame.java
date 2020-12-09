376cf79f7ed6e12eba403bac7857f2161c17fe8a
shiraalter <shiraalter@gmail.com>
2020-12-08 21:37:30 -0500

    added to frame

--- src/main/java/FC/Frame.java
+++ src/main/java/FC/Frame.java
@@ -1,15 +1,25 @@
 package FC;
 
+import javafx.scene.layout.BorderStroke;
+
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Frame extends JFrame {
 
-    private JComboBox<String> deckBox;
 
     private JPanel leftPanel;
     private JPanel chooseDeckPanel;
+    private JComboBox<String> deckBox;
+    private JButton newDeckButton;
+    private JPanel chooseButtonPanel;
+
     private JPanel existingDeckPanel;
+    private JPanel existingButtonPanel;
+    private JButton studyButton;
+    private JButton editButton;
+    private JButton deleteDeckButton;
 
     public Frame(){
         setSize(750,750);
@@ -19,10 +29,42 @@ public class Frame extends JFrame {
 
 
        leftPanel = new JPanel(new GridLayout(2,1));   //will hold two panels (new deck/existing & study/edit/delete deck)
-       chooseDeckPanel = new JPanel();
+       chooseDeckPanel = new JPanel(new BorderLayout());
+       chooseDeckPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+       existingDeckPanel = new JPanel(new BorderLayout());
+       existingDeckPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+       chooseButtonPanel = new JPanel(new GridLayout(2,1));
+       deckBox = new JComboBox<>();
+       newDeckButton = new JButton("New DecK");
+       chooseButtonPanel.add(deckBox);
+       chooseButtonPanel.add(newDeckButton);
+       chooseDeckPanel.add(chooseButtonPanel, BorderLayout.CENTER);
        leftPanel.add(chooseDeckPanel);
-       existingDeckPanel = new JPanel();
+
+       existingButtonPanel = new JPanel(new GridLayout(3,1));
+       studyButton = new JButton("Study");
+       studyButton.setBackground(Color.PINK);
+       editButton = new JButton("Edit Deck");
+       editButton.setBackground(Color.PINK);
+       deleteDeckButton = new JButton("Delete Deck");
+       deleteDeckButton.setBackground(Color.PINK);
+       existingButtonPanel.add(studyButton);
+       existingButtonPanel.add(editButton);
+       existingButtonPanel.add(deleteDeckButton);
+       existingDeckPanel.add(existingButtonPanel, BorderLayout.CENTER);
        leftPanel.add(existingDeckPanel);
+
+       existingDeckPanel.setVisible(false);
+
+       add(leftPanel, BorderLayout.WEST);
+
+       deckBox.addItem("HAPPY");
+       deckBox.addActionListener(actionEvent -> existingDeckPanel.setVisible(true));
+       newDeckButton.addActionListener(actionEvent -> existingDeckPanel.setVisible(false));
+
+
+
 
 
 
--- src/main/java/FC/Main.java
+++ src/main/java/FC/Main.java
@@ -3,5 +3,8 @@ package FC;
 public class Main {
     public static void main(String[] args) {
 
+    Frame frame = new Frame();
+    frame.setVisible(true);
+
     }
 }
