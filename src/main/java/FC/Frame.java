0101db1bf95193c721978b4211acc3d464794afd
shiraalter <shiraalter@gmail.com>
2020-12-08 20:45:23 -0500

    started frame

--- src/main/java/FC/Frame.java
+++ src/main/java/FC/Frame.java
@@ -1,2 +1,37 @@
-package FC;public class Frame {
+package FC;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+    private JComboBox<String> deckBox;
+
+    private JPanel leftPanel;
+    private JPanel chooseDeckPanel;
+    private JPanel existingDeckPanel;
+
+    public Frame(){
+        setSize(750,750);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Flashcard UI");
+        setLayout(new BorderLayout());
+
+
+       leftPanel = new JPanel(new GridLayout(2,1));   //will hold two panels (new deck/existing & study/edit/delete deck)
+       chooseDeckPanel = new JPanel();
+       leftPanel.add(chooseDeckPanel);
+       existingDeckPanel = new JPanel();
+       leftPanel.add(existingDeckPanel);
+
+
+
+
+
+
+
+
+    }
+
 }
+
