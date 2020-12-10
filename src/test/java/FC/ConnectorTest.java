0ea05981c844430cdbdf324184b2b48f40e3063b
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-12-09 23:05:10 -0500

    created connector test

--- src/test/java/FC/ConnectorTest.java
+++ src/test/java/FC/ConnectorTest.java
@@ -1,4 +1,26 @@
+package FC;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.Assert.*;
+
 public class ConnectorTest {
-  
+
+    @Test
+    public void connect() throws SQLException {
+        //given
+        String DB_FILE = "flash_cards.db";
+        Connector connector = new Connector(DB_FILE);
+
+        //when
+        Connection connection = connector.connect();
+
+        //then
+        assert(connection.isValid(0));
+
+    }
+
 }
