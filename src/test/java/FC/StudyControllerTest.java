67eb23d361ee127b6599697890c7ec7c2bfde90c
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-12-10 16:03:31 -0500

    created tests for StudyController

--- src/test/java/FC/StudyControllerTest.java
+++ src/test/java/FC/StudyControllerTest.java
@@ -1,4 +1,62 @@
+package FC;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.Assert.*;
+
 public class StudyControllerTest {
-  
+
+    @Test
+    public void getNextToStudy() throws SQLException {
+        //given
+        String deckName = "Sample";
+        StudyController studyController = new StudyController(deckName);
+
+        //when
+        Card card = studyController.getNextToStudy();
+
+        //then
+        assert (card.getDef() != null);
+        assert (card.getTerm() != null);
+        assert (card.getId() != null);
+    }
+
+    @Test
+    public void masterCard() throws SQLException {
+        //given
+        String deckName = "Sample";
+        StudyController studyController = new StudyController(deckName);
+        Card card = studyController.getDeck(deckName).getCard(1);
+        Deck deck = studyController.getUnMastered();
+
+        //when
+        studyController.masterCard(card);
+
+        //then
+        assert (deck.getSize() == 4);
+
+    }
+
+    @Test
+    public void startNewStudySession() throws SQLException {
+        //given
+        String deckName = "Sample";
+        StudyController studyController = new StudyController(deckName);
+        Deck deck = studyController.getUnMastered();
+        int deckSize = deck.getSize();
+
+        //when
+        for (int i = 0; i < deckSize; i++) {
+            Card card = studyController.getDeck(deckName).getCard(i);
+            studyController.masterCard(card);
+        }
+        studyController.startNewStudySession(deckName);
+        deck = studyController.getUnMastered();
+
+        //then
+        assertEquals (5, deck.getSize());
+
+    }
 }
