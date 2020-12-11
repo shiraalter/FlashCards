2cf6c80de0c78d33e159cc9a6819f37a37e4c0cd
shiraalter <shiraalter@gmail.com>
2020-12-10 20:53:28 -0500

    took out prints

--- src/main/java/FC/Main.java
+++ src/main/java/FC/Main.java
@@ -5,15 +5,6 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-/*        StudyController controller = new StudyController("Sample");
-        System.out.println("DB has the following deck: " + controller.getAllDecks()[0]);
-        System.out.println("Sample has the following random cards:\n" + controller.getNextToStudy().getTerm());
-        System.out.println(controller.getNextToStudy().getTerm());
-        System.out.println(controller.getNextToStudy().getTerm());
-        System.out.println(controller.getNextToStudy().getTerm());
-        System.out.println(controller.getNextToStudy().getTerm());
-        System.out.println(controller.getNextToStudy().getTerm());*/
-
         Frame frame = new Frame();
         frame.setVisible(true);
 
