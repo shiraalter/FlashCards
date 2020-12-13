488a866d428dd453928eae7e9e885ce88e37fbe8
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-12-12 23:39:56 -0500

    added new tests

--- src/test/java/FC/EditControllerTest.java
+++ src/test/java/FC/EditControllerTest.java
@@ -28,6 +28,28 @@ public class EditControllerTest {
     }
 
     @Test
+    public void insertCards() throws SQLException {
+        //given
+        String title = "Testing";
+        String term = "term";
+        String def = "def";
+        String term2 = "term 2";
+        String def2 = "definition with a lot of words to test out that it can handle it";
+        EditController editController = new EditController();
+
+        //when
+        editController.addDeck(title);
+        editController.insertCard(title, term, def);
+        editController.insertCard(title, term2, def2);
+        Deck deck = editController.getEditDeck();
+
+        //then
+        assertEquals(2, deck.getSize());
+        editController.deleteDeck(title);
+
+    }
+
+    @Test
     public void deleteCard() throws SQLException {
         //given
         String title = "Testing";
@@ -53,6 +75,22 @@ public class EditControllerTest {
         //given
         EditController editController = new EditController();
         String title = "Testing";
+
+        //when
+        editController.initializeNewDeck(title);
+
+        //then
+        assertNotNull(editController.getDeck(title));
+        editController.deleteDeck(title);
+
+    }
+
+
+    @Test
+    public void initializeNewDeckTwoWords() throws SQLException {
+        //given
+        EditController editController = new EditController();
+        String title = "Testing two words";
 
         //when
         editController.initializeNewDeck(title);
